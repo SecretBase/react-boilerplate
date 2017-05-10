e080b4cc9972ca33940c6a2271f3117b79afd930
SecretBase <whpigpig@gmail.com>
2017-05-10 23:54:17 +0800

    Remove bootstrap.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'normalize.css/normalize.css'
 
 import './globalStyle/global.scss'
 
